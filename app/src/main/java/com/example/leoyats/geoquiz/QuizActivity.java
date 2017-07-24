88c1c5caa45ee1f5edf749f6bc76b85b8332a003
leoyats <leoyats@gmail.com>
2017-07-24 16:38:55 +0300

    Add icon from buttons and replace Button to ImageButton

--- app/src/main/java/com/example/leoyats/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/leoyats/geoquiz/QuizActivity.java
@@ -4,14 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mPreviousButton;
-    private Button mNextButton;
+    private ImageButton mPreviousButton;
+    private ImageButton mNextButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionsBank = new Question[] {
             new Question(R.string.question_africa, false),
@@ -64,7 +65,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mPreviousButton = (Button) findViewById(R.id.previous_button);
+        mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
         mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,7 +79,7 @@ public class QuizActivity extends AppCompatActivity {
 
         updateQuestion();
 
-        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
